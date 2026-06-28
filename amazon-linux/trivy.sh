4db998e6a062e3f2aebbb605692389ebbe00b6f3
anand <anandgudewar857@gmail.com>
2026-06-28 11:45:55 +0530

    added trivy.sh

---
+++ amazon-linux/trivy.sh
@@ -0,0 +1,12 @@
+cat << EOF | sudo tee /etc/yum.repos.d/trivy.repo
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+sudo dnf -y update
+sudo dnf -y install trivy
+trivy --version
+
